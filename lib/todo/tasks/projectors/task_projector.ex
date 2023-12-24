2d66d5267fabab05e44bf125214cb7d91ffc0823
Liam McCartney <liam.mccartney@gmail.com>
2023-12-24 16:25:50 -0500

    set up projector

---
+++ lib/todo/tasks/projectors/task_projector.ex
@@ -0,0 +1,18 @@
+defmodule Todo.Tasks.Projectors.TaskProjector do
+  alias Todo.Tasks.Task
+  alias Todo.Tasks.Events.TaskCreated
+
+  use Commanded.Projections.Ecto,
+    name: "Todo.Tasks.Projectors.TaskProjector",
+    application: Todo.EventApplication,
+    # TODO: Move to eventual consistency
+    consitency: :strong
+
+  project(%TaskCreated{} = created_event, _, fn multi ->
+    Ecto.Multi.insert(multi, :task, %Task{
+      uuid: created_event.uuid,
+      title: created_event.title,
+      order: created_event.order
+    })
+  end)
+end
